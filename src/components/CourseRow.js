d1fd3eb072c5318bd08101effa468e0728aded2b
Nosakhare Emmanuel <nonreactiv@gmail.com>
2018-02-22 01:09:38 +0100

    Added editing mode to CourseRow component

--- src/components/CourseRow.js
+++ src/components/CourseRow.js
@@ -9,9 +9,12 @@ import {
   Title,
   FormGroup,
   Caption,
-  DropDownMenu
+  DropDownMenu,
+  TouchableOpacity
 } from '@shoutem/ui'
 import { GradeIndicator } from './GradeIndicator'
+import { editCourse, deregisterCourse, changeGrade } from '../redux/actions'
+import Collapsible from 'react-native-collapsible'
 
 const mapStateToProps = state => ({
   courses: state.courses,
@@ -19,33 +22,144 @@ const mapStateToProps = state => ({
 })
 
 const options = [
-  { grade: 'A', point: 5 },
-  { grade: 'B', point: 4 },
-  { grade: 'C', point: 3 },
-  { grade: 'D', point: 2 },
+  { grade: 'F', point: 0 },
   { grade: 'E', point: 1 },
-  { grade: 'F', point: 0 }
+  { grade: 'D', point: 2 },
+  { grade: 'C', point: 3 },
+  { grade: 'B', point: 4 },
+  { grade: 'A', point: 5 }
 ]
 
-const emptyOption = { id: '', name: 'Select' }
-const optionsWithEmptyOption = [emptyOption, ...options]
+// const emptyOption = { point: 0, grade: '-' }
+// const optionsWithEmptyOption = [emptyOption, ...options]
 
-class CourseRowX extends React.PureComponent {
-  constructor(props) {
-    super(props)
-    const score = this.getScore(props)
-
-    // if(score>)
-
-    this.state = {
-      selectedGrade: emptyOption
-    }
+class CourseRowX extends React.Component {
+  state = {
+    editingMode: false
   }
+
+  onChangeName = name =>
+    this.props.dispatch(editCourse({ id: this.props.id, name }))
+
+  onChangeCreditLoad = creditLoad =>
+    this.props.dispatch(
+      editCourse({ id: this.props.id, creditLoad: Number(creditLoad) })
+    )
+
+  onChangeGrade = grade => {
+    const { id, semester, year, dispatch } = this.props
+
+    dispatch(changeGrade({ id, semester, year, grade: grade.point }))
+  }
+
+  deregisterCourse = () => {
+    const { id, semester, year, dispatch } = this.props
+
+    dispatch(deregisterCourse({ id, semester, year }))
+  }
+
   getScore = ({ field, semester, year, id }) =>
     field.structure[`${year}$${semester}`][id]
+
+  toggleEditingMode = () =>
+    this.setState(oldState => ({ editingMode: !oldState.editingMode }))
+
+  renderNormalMode = ({
+    id,
+    courses,
+    semester,
+    year,
+    field,
+    course,
+    score,
+    selectedGrade
+  }) => (
+    <View
+      styleName="horizontal"
+      style={{
+        padding: 15,
+        backgroundColor: '#fff',
+        borderRadius: 10
+      }}
+    >
+      <View styleName="vertical flexible">
+        <Title>{course.name}</Title>
+        <Caption>credits: {course.creditLoad}</Caption>
+      </View>
+      <GradeIndicator point={score} />
+    </View>
+  )
+
+  renderEditingMode = ({
+    id,
+    courses,
+    semester,
+    year,
+    field,
+    course,
+    score,
+    selectedGrade
+  }) => (
+    <View
+      styleName="horizontal"
+      style={{
+        paddingTop: 15,
+        paddingBottom: 15,
+        backgroundColor: '#fff',
+        borderRadius: 10
+      }}
+    >
+      <View styleName="vertical flexible">
+        {/* <View styleName="horizontal"> */}
+        <FormGroup styleName="stretch">
+          <Caption>COURSE TITLE</Caption>
+          <TextInput
+            placeholder="..."
+            value={course.name}
+            onChangeText={this.onChangeName}
+          />
+        </FormGroup>
+        <View styleName="horizontal">
+          <FormGroup styleName="">
+            <Caption>CREDIT LOAD</Caption>
+            <TextInput
+              placeholder="..."
+              value={String(course.creditLoad)}
+              onChangeText={this.onChangeCreditLoad}
+            />
+          </FormGroup>
+          <FormGroup styleName="">
+            <Caption>GRADE</Caption>
+            <DropDownMenu
+              styleName={selectedGrade.point >= 0 ? '' : 'empty'}
+              options={options}
+              selectedOption={selectedGrade}
+              onOptionSelected={this.onChangeGrade}
+              titleProperty={'grade'}
+              valueProperty={'point'}
+            />
+          </FormGroup>
+        </View>
+        <FormGroup styleName="horizontal" style={{ marginRight: 15 }}>
+          <View styleName="flexible" />
+          <Button onPress={this.deregisterCourse}>
+            <Caption style={styles.red}>Remove</Caption>
+          </Button>
+
+          <Button onPress={this.toggleEditingMode}>
+            <Caption style={styles.blue}>Done</Caption>
+          </Button>
+        </FormGroup>
+        {/* </View> */}
+      </View>
+    </View>
+  )
+
   render() {
     const { id, courses, semester, year, field } = this.props
+    const { editingMode } = this.state
     const course = courses[id]
+
     if (!course)
       return (
         <Row>
@@ -54,35 +168,47 @@ class CourseRowX extends React.PureComponent {
       )
 
     const score = this.getScore(this.props)
-    const { selectedGrade } = this.state
+    const selectedGrade = options[score] || { point: 0, grade: '-' }
+
     return (
-      <Row>
-        <View styleName="vertical">
-          {/* <View styleName="horizontal"> */}
-          <FormGroup styleName="stretch">
-            {/* <Caption>COURSE TITLE</Caption> */}
-            <TextInput placeholder="..." value={course.name} />
-          </FormGroup>
-          {/* <FormGroup styleName="">
-            <Caption>Remove</Caption>
-          </FormGroup> */}
-          {/* <Caption>GRADE</Caption> */}
-          {/* <DropDownMenu
-              styleName={selectedGrade.point >= 0 ? '' : 'empty'}
-              options={optionsWithEmptyOption}
-              selectedOption={selectedGrade}
-              onOptionSelected={option =>
-                this.setState({ selectedGrade: option })
-              }
-              titleProperty={'grade'}
-              valueProperty={'point'}
-            /> */}
-          {/* </View> */}
+      <View
+        styleName="horizontal"
+        style={{
+          margin: 15,
+          // padding: 10,
+          backgroundColor: '#fff',
+          borderRadius: 10,
+          elevation: 1
+        }}
+      >
+        <View styleName="vertical flexible">
+          <Collapsible collapsed={editingMode}>
+            <TouchableOpacity onPress={this.toggleEditingMode}>
+              {this.renderNormalMode({
+                ...this.props,
+                course,
+                score,
+                selectedGrade
+              })}
+            </TouchableOpacity>
+          </Collapsible>
+          <Collapsible collapsed={!editingMode}>
+            {this.renderEditingMode({
+              ...this.props,
+              course,
+              score,
+              selectedGrade
+            })}
+          </Collapsible>
         </View>
-        <GradeIndicator point={score} />
-      </Row>
+      </View>
     )
   }
 }
 const CourseRow = connect(mapStateToProps)(CourseRowX)
 export { CourseRow }
+
+const styles = {
+  red: { color: '#f56' },
+  blue: { color: '#05f' }
+}
--- src/components/GradeIndicator.js
+++ src/components/GradeIndicator.js
@@ -4,11 +4,14 @@ import { View, Text } from 'react-native'
 // const initialScale = { a: 70, b: 65, c: 50, d: 45, f: 0 }
 const initialScale = { a: 5, b: 4, c: 3, d: 2, e: 1, f: 0 }
 
+const getColor = point =>
+  ['#f00', '#c47', '#000', '#0ff', '#05f', '#2f5'][point]
+
 export const getGradeFromPoint = (point, scale = initialScale) =>
   (Object.keys(scale).find(key => point === scale[key]) || '-').toUpperCase()
 
 export const GradeIndicator = ({ point }) => (
-  <View style={styles.bubble}>
+  <View style={[styles.bubble, { backgroundColor: '#ffd200' }]}>
     <Text style={styles.letter}>{getGradeFromPoint(point)}</Text>
   </View>
 )
@@ -19,11 +22,10 @@ const styles = {
     width: 60,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#50f',
     borderRadius: 30
   },
   letter: {
     fontSize: 30,
-    color: '#fff'
+    color: '#000'
   }
 }
