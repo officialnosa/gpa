1f5a384758aed77c707c5b3027fbd410663a288d
Emmanuel Nosakhare <nonreactiv@gmail.com>
2022-09-21 08:22:22 +0100

    fix: bump deps

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config')
-const { makeMetroConfig } = require('@rnx-kit/metro-config')
-const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks')
 
 const config = getDefaultConfig(__dirname)
 
 // Remove all console logs in production...
 config.transformer.minifierConfig.compress.drop_console = true
 
-module.exports = makeMetroConfig({
-  projectRoot: __dirname,
-  resolver: {
-    resolveRequest: MetroSymlinksResolver(),
-  },
-})
+module.exports = config
